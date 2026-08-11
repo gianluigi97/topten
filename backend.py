1f165d55b1f6db59da752bd93c4a922f5da5c406
gianluigi97 <gianluigi.mosti97@gmail.com>
2026-08-11 19:42:49 +0200

    inserimento classifiche in database

--- backend.py
+++ backend.py
@@ -1,17 +1,32 @@
 from flask import Flask, render_template, request
 from funcs.get_lists import ListTen
+from funcs.database import Database
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     categoria = request.args.get("categoria", "").strip()
     service = ListTen()
+    db = Database(database="Topten")
+
+    if not categoria:
+        return render_template(
+            "index.html",
+            classifica=[],
+            categoria=""
+        )
+
     raw_ranking = service.get_rank(richiesta=categoria)
+    db.insert(table_name="records", lista_ranking=service.format_ia_response(raw_ranking, categoria=categoria, db=True))
     classifica = service.format_ia_response(raw_ranking) if categoria else []
-    return render_template("index.html", classifica=classifica, categoria=categoria)
 
-
+    return render_template(
+        "index.html", 
+        classifica=classifica, 
+        categoria=categoria
+        )
 
 
 
--- funcs/database.py
+++ funcs/database.py
@@ -1,27 +1,62 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, insert, MetaData, Table
 from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 import os
 
-load_dotenv("/Users/gianluigimosti/WorkPlace/topten/key.env")
-USERNAME = os.getenv("USERNAME")
-PASSWORD = os.getenv("PASSWORD") 
-HOST = os.getenv("HOST")
-PORT = os.getenv("PORT")
-DB = os.getenv("DB")
+load_dotenv(r"C:\GianC\topten\key.env")
+
+class Database:
+
+    def __init__(self, database):
+
+        self.username = os.getenv("DB_USERNAME")
+        self.password = os.getenv("DB_PASSWORD") 
+        self.host = os.getenv("DB_HOST")
+        self.port = os.getenv("DB_PORT")
+        self.db = database
+
+        self._engine = None
+        self._metadata = MetaData()
+        self._tables = {}
+
+    def _get_engine(self):
+
+        if self._engine: 
+            return self._engine
+
+        self._engine = create_engine(
+            f"mysql+pymysql://{self.username}:{self.password}@{self.host}:{self.port}/{self.db}",
+            connect_args={'connect_timeout':5}
+        )
+
+        return self._engine
+
+    def _get_table(self, table_name):
+
+        if table_name in self._tables:
+            return self._tables[table_name]
+
+        table = Table(
+            table_name,
+            self._metadata,
+            autoload_with=self._get_engine()
+        )
+
+        self._tables[table_name] = table
+        return table
 
 
+    def insert(self, table_name, lista_ranking: dict):
 
-engine = create_engine(
-    f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB}",
-    connect_args={"connect_timeout": 5}
-)
+        if not lista_ranking: 
+            return 
 
-print("Prima di connect")
+        for el in lista_ranking: 
+            el.pop('descrizione', None)
 
-with engine.connect() as conn:
-    print("Connesso")
-    print("Prima di SELECT")
-    result = conn.execute(text("SELECT 1"))
-    print("Dopo SELECT")
-    print(result.fetchall())
+        table = self._get_table(table_name)
+        stmt = insert(table)
+
+        with self._get_engine().begin() as conn:
+            conn.execute(stmt, lista_ranking)
+
